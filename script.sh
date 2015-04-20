2962869b7a3c558d2f1f27d5359832d34775d20d
ffacon <francois.facon@gmail.com>
2015-04-20 10:59:53 +0200

    Update Script.sh

--- script.sh
+++ script.sh
@@ -10,26 +10,27 @@ fi
 
 echo "Creation des repertoires"
 mkdir www/
-mkdir www/css
-mkdir www/data
-mkdir www/img
-mkdir www/js
-mkdir www/lib
-mkdir www/views
+mkdir www/app
+mkdir www/app/css
+mkdir www/app/data
+mkdir www/app/img
+mkdir www/app/js
+mkdir www/app/libs
+mkdir www/app/views
 
 echo "Copie des fichiers html"
-cp dev/app/*.html www/
-cp -r dev/app/views/* www/views
+cp dev/app/*.html www/app
+cp -r dev/app/views/* www/app/views
 
 echo "Copie des data"
-cp -r dev/app/data/* www/data
+cp -r dev/app/data/* www/app/data
 echo "Copie des images"
-cp -r dev/app/img/* www/img
+cp -r dev/app/img/* www/app/img
 
 echo "Copie des  autres fichier "
-cp -r dev/app/css/* www/css
-cp -r dev/app/js/* www/js
-cp -r dev/app/lib/* www/lib
+cp -r dev/app/css/* www/app/css
+cp -r dev/app/js/* www/app/js
+cp -r dev/app/libs/* www/app/libs
 
 
 echo "-- L'application TechForum 2015 contenu dans le dossier \"www\" peut être mise en production --"
